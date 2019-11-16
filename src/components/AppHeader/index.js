63bb9cda6e450bdf3bda6c345883a06caa4a197d
Oriana <oriru92@gmail.com>
2019-11-16 18:18:01 -0300

    Ajusta el header de la aplicación para añadir los componentes necesarios.

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import AppBrandLogo from "components/AppBrandLogo";
+import SearchBar from "components/AppHeader/components/SearchBar";
+
 import './styles.scss';
 
 function AppHeader() {
@@ -8,6 +10,9 @@ function AppHeader() {
     <nav className="app-header">
       <div className="app-header__content container">
           <AppBrandLogo classNames={'app-header-logo'} />
+          <div className="app-header__search-bar">
+            <SearchBar />
+          </div>
       </div>
     </nav>
   );
