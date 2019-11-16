d963d23f4b128d1ecda1999f08ceb4d82a68e908
Oriana Ruiz <oriru92@gmail.com>
2019-11-16 13:57:42 -0300

    Agrega uso de componente header en vista principal.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,12 @@ import logo2x from 'assets/img/Logo_ML@2x.png';
 import 'App.scss';
 import {Route, Switch} from "react-router-dom";
 import routes from "routes";
+import AppHeader from "components/AppHeader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo2x} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <AppHeader />
       <Switch>
         {
           routes.map(({path, Component}) => (
@@ -30,7 +18,7 @@ function App() {
           ))
         }
       </Switch>
-    </div>
+    </>
   );
 }
 
