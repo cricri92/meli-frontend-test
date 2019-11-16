70f3778882710eb7e222ab9c175e9736c7e51aba
Oriana Ruiz <oriru92@gmail.com>
2019-11-16 13:57:08 -0300

    Crea componente básico para header de aplicación.

---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
+
+function AppHeader() {
+  return (
+    <div>
+        AppHeader component!
+    </div>
+  );
+}
+
+AppHeader.propTypes = {
+  //
+};
+
+export default AppHeader;
