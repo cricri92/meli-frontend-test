3b84dc3fc09a09443a4f4c9a80eac429dc2da74f
Oriana Ruiz <oriru92@gmail.com>
2019-11-16 13:42:12 -0300

    Crea vista básica para detalle de producto.

---
+++ src/pages/ProductDetail/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+function ProductDetail() {
+  return (
+    <div>
+      Product detail page!
+    </div>
+  );
+}
+
+export default ProductDetail;
