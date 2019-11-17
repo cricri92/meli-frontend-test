134ac5fbce701fd948b7c48ae20d4cb293c669f5
Oriana <oriru92@gmail.com>
2019-11-17 20:07:58 -0300

    Ajustes varios de estilos en listado de productos.

--- src/pages/products/components/ProductListItem/index.js
+++ src/pages/products/components/ProductListItem/index.js
@@ -15,34 +15,34 @@ function ProductListItem({ product }) {
   const productLink = `/items/${product.id}`;
 
   return (
-    <li className="product-list-item">
+    <li className="product-list-item divider divider--min">
       <Link to={productLink} className="product-list-item-link">
         <AppThumbnail
           thumbnail={product.thumbnail}
           title={product.title}
           classNames="product-list-item__thumbnail" />
       </Link>
-        <div className="product-list-item-content">
-          <div className="product-info">
-            <div className="product-info__item">
-              <ProductPrice classNames="product-price--list"
+      <div className="product-list-item-content">
+        <div className="product-info">
+          <div className="product-info__item">
+            <ProductPrice classNames="product-price--list"
                 price={product.price}
                 currency={product.currency_id}
                 priceAddons={
                   product.shipping.free_shipping &&
                     <AppIcon iconType={SHIPPING_ICON} description="Free shipping"/>
                 } />
-            </div>
-            <div className="product-info__item">
-              <Link to={productLink} className="product-list-item-link">
-                <AppSecondaryTitle title={product.title} />
-              </Link>
-            </div>
           </div>
-          <span className="product-location">
-            {product.address.state_name}
-          </span>
+          <div className="product-info__item">
+            <Link to={productLink} className="product-list-item-link">
+              <AppSecondaryTitle title={product.title} />
+            </Link>
+          </div>
         </div>
+        <span className="product-location">
+          {product.address.state_name}
+        </span>
+      </div>
     </li>
   );
 }
