29efd7f23c221e9cbc6c678d48ec217182844aad
Oriana <oriru92@gmail.com>
2019-11-18 00:22:09 -0300

    Ajusta enlace a icono para obtenerlo desde los statics del backend.

--- client/src/components/AppBrandLogo/index.js
+++ client/src/components/AppBrandLogo/index.js
@@ -3,14 +3,14 @@ import {Link} from "react-router-dom";
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 
-import brandLogo2x from 'assets/img/Logo_ML@2x.png';
-
 import './styles.scss';
 
 function AppBrandLogo({ classNames }) {
+  const url = process.env.REACT_APP_BACKEND_URL;
+
   return (
     <Link to='/'>
-      <img src={brandLogo2x}
+      <img src={`${url}assets/img/Logo_ML@2x.png`}
            alt="Mercado Libre, todo en un solo lugar"
            className={
              classnames({
