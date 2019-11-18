d2d46349ce16b2f463d1a59643dc9423f83e4852
Oriana <oriru92@gmail.com>
2019-11-18 18:41:47 -0300

    Instala y configura CORS para habiltar uso de la API.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
-
+const cors = require('cors');
 const itemsRouter = require('./routes/items');
 
 const app = express();
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/items', itemsRouter);
 
