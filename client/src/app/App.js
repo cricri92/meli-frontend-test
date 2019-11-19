a9b12569b11f223d82bd6245be85f760e57f34d2
Oriana <oriru92@gmail.com>
2019-11-18 23:34:57 -0300

    Agrega ejemplo de uso de API.

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -3,8 +3,19 @@ import 'app/App.scss';
 import {Route, Switch} from "react-router-dom";
 import routes from "routes";
 import AppHeader from "components/AppHeader";
+import SearchService from "../services/Search";
+import ItemsService from "../services/Items";
 
 function App() {
+  async function init() {
+    const items = await SearchService.getResults('iphone');
+    const item = await ItemsService.getItem('MLA825811914');
+
+    console.log(items,item);
+  }
+
+  init();
+
   return (
     <>
       <AppHeader />
