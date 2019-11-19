8f13bb440d9262fadbac0592013a88ac1af856b1
Oriana <oriru92@gmail.com>
2019-11-19 02:04:28 -0300

    Ajusta componentes de listado e items de listado de resultados de busqueda.

--- client/src/pages/products/components/ProductsList/index.js
+++ client/src/pages/products/components/ProductsList/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import AppMainTitle from "components/AppMainTitle";
+
 import ProductListItem from "pages/products/components/ProductListItem";
 
 import './styles.scss';
 
 function ProductsList({ productsList }) {
   return (
-    <ul className="products-list">
-        {
-          productsList.map(product =>
-            <ProductListItem product={product} key={product.id} />
-          )
-        }
-    </ul>
+    productsList.length > 0 ?
+      <ul className="products-list">
+          {
+            productsList.map(product =>
+              <ProductListItem product={product} key={product.id} />
+            )
+          }
+      </ul>
+    :
+      <AppMainTitle title='No existen resultados disponibles para esta búsqueda' />
   );
 }
 
