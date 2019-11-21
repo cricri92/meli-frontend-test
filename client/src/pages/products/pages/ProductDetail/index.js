7c1efce49c5e96b7498f44bb553a3c290c562165
Oriana <oriru92@gmail.com>
2019-11-21 00:35:41 -0300

    Modifica vista de detalle de item para agregar mensaje de error si existe.

--- client/src/pages/products/pages/ProductDetail/index.js
+++ client/src/pages/products/pages/ProductDetail/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {withRouter} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import {withRouter, useParams} from "react-router-dom";
 
 import AppInnerPage from "components/AppInnerPage";
 import AppThumbnail from "components/AppThumbnail";
@@ -7,6 +7,7 @@ import AppMainTitle from "components/AppMainTitle";
 import AppButton from "components/AppButton";
 
 import {PRIMARY_BUTTON} from "components/AppButton/constants";
+import {PRODUCT_DETAIL_ADD_TO_CART_BUTTON_TEXT, PRODUCT_DETAIL_LOADING_TEXT} from "./constants";
 
 import ProductConditions from "pages/products/components/ProductConditions";
 import ProductPrice from "pages/products/components/ProductPrice";
@@ -17,62 +18,67 @@ import ItemsService from "services/Items";
 
 import './styles.scss';
 
-class ProductDetail extends Component {
-  state = {
-    product: null
-  };
+function ProductDetail() {
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
 
-  async componentDidMount() {
-    const { match } = this.props;
-    const { params } = match;
-    const { id } = params;
+  useEffect(() => {
+    if (!product) {
+      ItemsService.getItem(id)
+      .then(product => {
+        setProduct(product)
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  });
 
-    const product = await ItemsService.getItem(id);
-
-    this.setState({
-      product
-    });
-  }
-
-  handleBuyProductClick() {
+  function handleBuyProductClick() {
     console.log('buying product');
   }
 
-  render() {
-    const {product} = this.state;
-
-    return (
-      product ?
+  return (
         <AppInnerPage classNames="product-detail">
-          <div className="product-detail-content">
-            <AppThumbnail title={product.title}
-                          thumbnail={product.picture}
-                          classNames="product-detail-content__thumbnail"/>
-             <div className="product-detail-info">
-              <ProductConditions product={product}/>
-              <div className="divider divider--min">
-                <AppMainTitle title={product.title} />
-              </div>
-               <div className="divider divider--max">
-                 <ProductPrice price={product.price}
-                               classNames="product-detail-info__price" />
-               </div>
-               <div className="divider divider--max">
-                 <AppButton buttonType={PRIMARY_BUTTON}
-                            actionToExecute={this.handleBuyProductClick}>
-                   Comprar
-                 </AppButton>
-               </div>
-             </div>
-          </div>
-          <div className="divider divider--max">
-            <ProductDetailDescription description={product.description} />
-          </div>
+          {
+            !product && <AppMainTitle title={PRODUCT_DETAIL_LOADING_TEXT} />
+          }
+          {
+            product && product.error && (
+              <AppMainTitle title={product.message} />
+            )
+          }
+          {
+            product && product.id && (
+              <>
+                <div className="product-detail-content">
+                  <AppThumbnail title={product.title}
+                                thumbnail={product.picture}
+                                classNames="product-detail-content__thumbnail"/>
+                   <div className="product-detail-info">
+                    <ProductConditions product={product}/>
+                    <div className="divider divider--min">
+                      <AppMainTitle title={product.title} />
+                    </div>
+                     <div className="divider divider--max">
+                       <ProductPrice price={product.price}
+                                     classNames="product-detail-info__price" />
+                     </div>
+                     <div className="divider divider--max">
+                       <AppButton buttonType={PRIMARY_BUTTON}
+                                  actionToExecute={handleBuyProductClick}>
+                         {PRODUCT_DETAIL_ADD_TO_CART_BUTTON_TEXT}
+                       </AppButton>
+                     </div>
+                   </div>
+                </div>
+                <div className="divider divider--max">
+                  <ProductDetailDescription description={product.description} />
+                </div>
+              </>
+            )
+          }
         </AppInnerPage>
-      :
-        null
     );
-  }
 }
 
 export default withRouter(ProductDetail);
